626c3aa33dae0de8f881e6b8066ebce0f0395398
RL <subwurzburg24@gmail.com>
2024-12-24 02:58:11 +0800

    test

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,8 @@ import { createPinia } from 'pinia'; // 引入 Pinia
 import App from './App.vue';
 import './style.css';
 
-// 創建 Vue 應用
 const app = createApp(App);
 
-// 安裝插件
 const pinia = createPinia();
-app.use(pinia); // 使用 Pinia 作為全局狀態管理
-
-// 掛載應用
+app.use(pinia);
 app.mount('#app');
